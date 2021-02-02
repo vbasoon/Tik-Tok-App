b9bae96bb574d744ed38911fa7d967065bb4c4e7
Басун Володимир <vbasoon@gmail.com>
2021-02-02 11:42:29 +0200

    route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+import './index.css'
 
+const App = () => {
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path='/' component={Home} />
+        <Route path='/upload' component={Upload} />
+      </Switch>
+    </HashRouter>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
