906631692c3c73dd4b95ed72d06c17e5e626a042
Басун Володимир <vbasoon@gmail.com>
2021-02-02 13:39:27 +0200

    switch

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Upload from './pages/Upload'
-import './index.css'
+import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <HashRouter>
+      <Header />
       <Switch>
-        <Route path='/' exact component={Home} />
         <Route path='/upload' component={Upload} />
+        <Route path='/' exact component={Home} />
       </Switch>
+      <Footer />
     </HashRouter>
   );
 }
